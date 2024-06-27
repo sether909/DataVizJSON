f35995225e5a2f426a862cb9ad20eef292c278ae
sether909 <seth_reese1@baylor.edu>
2024-06-26 23:46:48 -0500

    Made improvements to JSON proj

--- mapcolleges.py
+++ mapcolleges.py
@@ -3,25 +3,32 @@ import json
 
 import plotly.express as px
 
+#Load the JSON data
+Big12_Unis = []
+with open('univ.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    for university in data:
+        naia_info = university['NCAA']
+        if naia_info["NAIA conference number football (IC2020)"] == 108:
+            Big12_Unis.append(university)
+
 geojson_file = Path('schools.geojson') 
-json_file = Path('univ.json')
-title_to_find = "Central High School"
 contents = Path.read_text(encoding='utf-8')
 data = json.loads(contents)
 
-# Examine all earthquakes in the dataset.
+# Examine in the dataset.
 all_eq_dicts = data['features']
 
 mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['NAME']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    eq_title = eq_dict['properties']['NAME']
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    eq_titles.append(eq_title)
+    if eq_dict['properties']['NAME'] in Big12_Unis:
+        eq_title = eq_dict['properties']['NAME']
+        lon = eq_dict['geometry']['coordinates'][0]
+        lat = eq_dict['geometry']['coordinates'][1]
+        mags.append(mag)
+        lons.append(lon)
+        lats.append(lat)
+        eq_titles.append(eq_title)
 
 title = 'Big 12 Schools Graph'
 fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
